48c975beeb6fae4c2af2e0ddde101ac3bcbc2a13
saniyagodil <saniyagodil@gmail.com>
2020-10-10 19:40:27 -0400

    Basic echo server client

--- client.py
+++ client.py
@@ -1,18 +1,18 @@
 import socket
 
-    def client_program():
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ('localhost', 1000)
-        print 'starting up on %s port %s' % server_address
-        s.connect(server_address)
-        print 'enter message'
-        message = input()
-        while message.lower() != 'bye':
-            s.send(message)
-            resp = s.recv()
-            print 'Recieved from server', resp
-            message = input("Enter Message")
+def client_program():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'localhost'
+    port = 1000
+    server_address = (host, port)
+    print('starting up on %s port %s' % server_address)
+    s.connect(server_address)
+    message = input("Enter Message")
+    while message.lower() != 'bye':
+        s.send(message.encode())
+        resp = s.recv(1024).decode()
+        print('Recieved from server', resp)
+        message = input("Enter Message: ")
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,23 +1,23 @@
 import socket
 
 def server_program():
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = 'localhost'
-    port = 10000
+    port = 1000
     server_address = (host, port)
-    print 'starting up on %s port %s' % server_address
+    print('starting up on %s port %s' % server_address)
     s.bind(server_address)
-#Binds to specific ip and port, can now listen to incoming connections
-    print 'waiting for a connection'
-    s.listen(2)
+    print('waiting for a connection')
+    s.listen(3)
 
     while True:
-
         connection, client_address = s.accept()
-        print 'Got connection from', client_address
-        data = conn.recv()
-        conn.sendall(data)
+        print('Got connection from', client_address)
+
+        while True:
+            data = connection.recv(1024).decode()
+            print('User:', client_address, 'Sent:', str(data))
+            connection.sendall(data.encode())
 
 if __name__ == '__main__':
     server_program()
