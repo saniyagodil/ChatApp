13cb19c4a36e5756198512ae1eadccd615648ae7
saniyagodil <saniyagodil@gmail.com>
2020-10-12 23:35:00 -0400

    Small tweaks, handling client closing issues

--- client.py
+++ client.py
@@ -1,31 +1,31 @@
 import socket
 import threading
 
+HOST = 'localhost'
+PORT = 1000
 
 def client_program():
-    HOST = 'localhost'
-    PORT = 1000
-    user_name = input('Enter name: ')
+    print(f'{"Welcome to the Chat App!": ^50}')
+    user_name = input(f'{"Enter name: ": ^15}')
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
-    print('starting up on %s port %s' % server_address)
     client.connect(server_address)
     client.send(user_name.encode())
-    threading.Thread(target = recieve_message, args = (client, "")).start()
-    threading.Thread(target = send_message, args = (client, "")).start()
+    threading.Thread(target = recieve_message, args = (client, )).start()
+    threading.Thread(target = send_message, args = (client, )).start()
 
-def recieve_message(connection, x):
+def recieve_message(connection):
     while True:
         message = connection.recv(4096).decode()
         if not message:
             break
         print(message)
 
-
-def send_message(connection, x):
+def send_message(connection):
     while True:
-        message = input("Enter Message: ")
-        if message.lower() != 'bye':
+        print("Enter Message: ")
+        message = input()
+        if message != 'bye':
             connection.send(message.encode())
         else:
             break
--- server.py
+++ server.py
@@ -4,26 +4,26 @@ import threading
 HOST = 'localhost'
 PORT = 1000
 list_of_clients = []
-server = None
 
 def server_program():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print('starting up on %s port %s' % server_address)
     server.bind(server_address)
-    server.listen(10)
-    threading.Thread(target = accept_clients, args = (server, "")).start()
+    server.listen(20)
+    threading.Thread(target = accept_clients, args = (server, )).start()
 
-def accept_clients(this_server, x):
+def accept_clients(this_server):
     while True:
         client, client_address = this_server.accept()
         print('Got connection from', client_address)
         client_name = client.recv(4096).decode()
         client.send((f"Welcome {client_name}").encode())
         list_of_clients.append((client_name, client))
-        threading.Thread(target = client_message, args = (client, client_address)).start()
+        threading.Thread(target = client_message, args = (client, )).start()
 
-def client_message(client, address):
+def client_message(client):
     while True:
         data = client.recv(4096).decode()
         if not data or data.lower() == 'bye':
@@ -32,14 +32,15 @@ def client_message(client, address):
         message = (sender_name + ": " + data).encode()
         send_everyone_message(message, client)
 
-    index = get_client_index(client)
-    del list_of_clients[index]
-    client.close()
+    clean_up_conn(client)
 
 def send_everyone_message(message, sender):
     for client_name, client in list_of_clients:
         if client != sender:
-            client.send(message)
+            try:
+                client.send(message)
+            except:
+                continue
 
 def get_name_given_connection(connection):
     for client_name, client in list_of_clients:
@@ -50,6 +51,12 @@ def get_client_index(connection):
     for index, client_name, client in enumerate(list_of_clients):
         if client == connection:
             return index
+
+def clean_up_conn(connection):
+    index = get_client_index(connection)
+    print(index)
+    del list_of_clients[index]
+    connection.close()
 
 if __name__ == '__main__':
     server_program()
